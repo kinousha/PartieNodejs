513fb029047c399b5806263006919b9999c0a5b6
kinousha <kinekhouma@esp.sn>
2022-05-09 10:31:58 +0000

    modification du fichier server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("./app/routes/todo.routes")(app);
+require("./app/routes/user.routes")(app);
 
 var corsOptions = {
   origin: "http://localhost:8081"
